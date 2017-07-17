a175dc8d9182f0b19586cfa6f70f1154046c763e
HugoVP <dev@hugo.plus>
2017-07-17 17:46:07 -0500

    Drawing default objects' views

--- Fuzzy-Logic-App/ViewController.swift
+++ Fuzzy-Logic-App/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    /* Canvas view */
+    @IBOutlet weak var canvas: UIView!
+    
+    /* Objects' views */
+    private var tunnelView: UIView!
+    private var targetView: UIView!
+    private var ballView: UIView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        /* Drawing views */
+        let tunnelViewPadding: CGFloat = 16
+        let ballViewSize: CGFloat = 40
+        
+        /* Setting Tunnel View */
+        tunnelView = UIView(
+            frame: CGRect(
+                x: canvas.bounds.midX - ballViewSize,
+                y: canvas.bounds.minY + tunnelViewPadding,
+                width: ballViewSize * 2,
+                height: canvas.bounds.maxY - tunnelViewPadding * 2
+            )
+        )
+        
+        tunnelView.backgroundColor = UIColor.darkGray
+        tunnelView.layer.borderWidth = 1
+        tunnelView.layer.borderColor = UIColor.black.cgColor
+        
+        /* Setting Target View */
+        targetView = UIView(
+            frame: CGRect(
+                x: canvas.bounds.midX - ballViewSize * 0.75,
+                y: canvas.bounds.midY - ballViewSize / 2,
+                width: ballViewSize * 1.5,
+                height: ballViewSize
+            )
+        )
+        
+        targetView.backgroundColor = .lightGray
+        targetView.layer.borderWidth = 1
+        targetView.layer.borderColor = UIColor.black.cgColor
+        targetView.layer.cornerRadius = ballViewSize / 2
+        
+        /* Setting Ball View */
+        ballView = UIView(
+            frame: CGRect(
+                x: canvas.bounds.midX - ballViewSize / 2,
+                y: canvas.bounds.midY - ballViewSize / 2,
+                width: ballViewSize,
+                height: ballViewSize
+            )
+        )
+        
+        ballView.backgroundColor = .cyan
+        ballView.layer.borderWidth = 1
+        ballView.layer.borderColor = UIColor.black.cgColor
+        ballView.layer.cornerRadius = ballViewSize / 2
+        
+        /* Setting Adding views */
+        canvas.addSubview(tunnelView)
+        canvas.addSubview(targetView)
+        canvas.addSubview(ballView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
